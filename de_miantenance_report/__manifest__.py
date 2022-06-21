59f1e040626d77b783bc148f6052310666215e7d
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2022-06-21 15:33:10 +0500

    add de_maintenance_report

---
+++ de_miantenance_report/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Maintenance Report",
+
+    'summary': """
+                Maintenance Report Piviot View
+                """,
+
+    'description': """
+        Maintenance Report Piviot View
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+
+    'category': 'Sales',
+    'version': '12.0.0.',
+
+    # any module necessary for this one to work correctly
+    'depends': ['maintenance'],
+
+    # always loaded
+    'data': [
+        # 'views/purchase_report_view.xml',
+        # 'report/sale_gain_report_template.xml',
+    ],
+
+}
