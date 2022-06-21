3ab7a3de42da9e8299fa81aab6ebb5ee414ba948
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2022-06-21 18:15:09 +0500

    update maintenance_report

--- de_miantenance_report/__manifest__.py
+++ de_miantenance_report/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '14.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['maintenance'],
+    'depends': ['maintenance', 'de_equipment_maintenance'],
 
     # always loaded
     'data': [
