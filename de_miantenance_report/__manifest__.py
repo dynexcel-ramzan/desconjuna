9a8d90d8481e605ffc5ee7848c50c6c911a99f15
ahmadraza47 <38661176+ahmadraza47@users.noreply.github.com>
2022-06-23 19:45:25 +0500

    update de_maintenace_report

--- de_miantenance_report/__manifest__.py
+++ de_miantenance_report/__manifest__.py
@@ -17,10 +17,11 @@
     'version': '14.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['maintenance', 'de_equipment_maintenance'],
+    'depends': ['maintenance', 'de_equipment_maintenance', 'de_account_maintenance_order'],
 
     # always loaded
     'data': [
+        'security/ir.model.access.csv',
         'views/purchase_report_view.xml',
         # 'report/sale_gain_report_template.xml',
     ],
--- de_miantenance_report/report/purchase_report.py
+++ de_miantenance_report/report/purchase_report.py
@@ -11,12 +11,16 @@ from odoo.exceptions import UserError
 from odoo.osv.expression import AND, expression
 
 
-class MaintenanceReport(models.Model):
-    _name = "maintenance.report"
+class MaintenanceOrderReport(models.Model):
+    _name = "maintenance.order.report"
     _description = "Maintenance Report"
-    #     _auto = False
+    _auto = False
+
     #     _rec_name = 'date'
     #     _order = 'date desc'
+    @api.model
+    def _get_done_states(self):
+        return ['confirm', 'inprocess', 'done', 'cancel']
 
     name = fields.Char('Order Reference', readonly=True)
     schedule_start_date = fields.Date(string='Schedule Start Date', readonly=True)
@@ -30,13 +34,16 @@ class MaintenanceReport(models.Model):
     qty_demand = fields.Float('Dmeand', readonly=True)
     qty_done = fields.Float('Issued', readonly=True)
     company_id = fields.Many2one('res.company', 'Company', readonly=True)
-    user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
+    user_id = fields.Many2one('res.users', 'Responsible', readonly=True)
+    currency_id = fields.Many2one('res.currency', 'Currency', readonly=True)
     price_total = fields.Float('Total', readonly=True)
-    price_subtotal = fields.Float('Untaxed Total', readonly=True)
+    price_subtotal = fields.Float('Subtotal', readonly=True)
+    price_unit = fields.Float('Cost', readonly=True)
     product_tmpl_id = fields.Many2one('product.template', 'Product', readonly=True)
     categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
     nbr = fields.Integer('# of Lines', readonly=True)
     analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', readonly=True)
+    #     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags', readonly=True)
     state = fields.Selection([
         ('draft', 'Draft'),
         ('confirm', 'Confirm'),
@@ -47,19 +54,19 @@ class MaintenanceReport(models.Model):
 
     order_id = fields.Many2one('maintenance.order', 'Order #', readonly=True)
 
-    def _select_sale(self, fields=None):
+    def _select_maintenance(self, fields=None):
         if not fields:
             fields = {}
         select_ = """
-            m.id as order_id,
             min(ml.id) as id,
             ml.product_id as product_id,
             t.uom_id as product_uom,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.demand_qty / u.factor * u2.factor) ELSE 0 END as qty_demand,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.done_qty / u.factor * u2.factor) ELSE 0 END as qty_done,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_subtotal / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_subtotal,
+            CASE WHEN ml.product_id IS NOT NULL THEN sum(ml.demand_qty) ELSE 0 END as qty_demand,
+            CASE WHEN ml.product_id IS NOT NULL THEN sum(ml.done_qty) ELSE 0 END as qty_done,
+            CASE WHEN ml.product_id IS NOT NULL THEN sum(standard_price) ELSE 0 END as price_unit,
             count(*) as nbr,
             m.name as name,
+            ml.currency_id as currency_id,
             m.schedule_start_date as schedule_start_date,
             m.schedule_end_date as schedule_end_date,
             m.start_date as start_date,
@@ -68,7 +75,8 @@ class MaintenanceReport(models.Model):
             m.user_id as user_id,
             m.company_id as company_id,
             m.equipment_id as equipment_id,
-            m.analytic_account_id as analytic_account_id,
+            analytic_account.id as analytic_account_id,
+            p.product_tmpl_id as product_tmpl_id,
             t.categ_id as categ_id,
             m.id as order_id
         """
@@ -77,35 +85,45 @@ class MaintenanceReport(models.Model):
             select_ += field
         return select_
 
-    def _from_sale(self, from_clause=''):
+    def _from_maintenance(self, from_clause=''):
         from_ = """
                 maintenance_order_line ml
-                      right outer join maintenance_order m on (m.id=ml.order_id)
+                      right outer join maintenance_order m on (m.id=ml.maintenance_line)
                         left join product_product p on (ml.product_id=p.id)
                         left join maintenance_equipment me on (m.equipment_id=me.id)
                             left join product_template t on (p.product_tmpl_id=t.id)
                     left join uom_uom u on (u.id=ml.product_uom)
                     left join uom_uom u2 on (u2.id=t.uom_id)
-                    left join account_analytic_account analytic_account on (m.account_analytic_id = analytic_account.id)
+                    left join account_analytic_account analytic_account on (ml.analytic_account_id = analytic_account.id)
+                    left join res_currency_rate cr on (cr.currency_id = m.currency_id and
+                        cr.company_id = m.company_id)
+                    left join {currency_table} ON currency_table.company_id = m.company_id
+
+
                 %s
-        """ % from_clause
+        """.format(
+            currency_table=self.env['res.currency']._get_query_currency_table(
+                {'multi_company': True, 'date': {'date_to': fields.Date.today()}}),
+        ) % from_clause
         return from_
 
-    def _group_by_sale(self, groupby=''):
+    def _group_by_maintenance(self, groupby=''):
         groupby_ = """
             ml.product_id,
-            ml.order_id,
+            ml.maintenance_line,
             t.uom_id,
-            t.categ_id,
             m.name,
+            ml.currency_id,
             m.schedule_start_date,
             m.schedule_end_date,
             m.start_date,
             m.end_date,
             m.user_id,
             m.state,
+            p.product_tmpl_id,
+            t.categ_id,
             m.company_id,
-            m.analytic_account_id,
+            analytic_account.id,
             m.id %s
         """ % (groupby)
         return groupby_
@@ -114,10 +132,11 @@ class MaintenanceReport(models.Model):
         if not fields:
             fields = {}
         with_ = ("WITH %s" % with_clause) if with_clause else ""
-        return '%s (SELECT %s FROM %s WHERE l.display_type IS NULL GROUP BY %s)' % \
-               (with_, self._select_sale(fields), self._from_sale(from_clause), self._group_by_sale(groupby))
+        return '%s (SELECT %s FROM %s WHERE ml.x_display_type IS NULL GROUP BY %s)' % \
+               (with_, self._select_maintenance(fields), self._from_maintenance(from_clause),
+                self._group_by_maintenance(groupby))
 
-#     def init(self):
-#         # self._table = sale_report
-#         tools.drop_view_if_exists(self.env.cr, self._table)
-#         self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+    def init(self):
+        #         self._table = maintenance_order_report
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
